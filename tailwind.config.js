2f94a25830676c9606131abe99a0d1519cf3a8c4
dev88260 <devyadav88260@gmail.com>
2024-12-07 19:47:34 -0800

    changes related to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,3 @@ export default {
   },
   plugins: [],
 };
-module.exports = {
-  theme: {
-    extend: {
-      animation: {
-        'slide-up': 'slideUp 0.5s ease-in-out',
-      },
-      keyframes: {
-        slideUp: {
-          '0%': { transform: 'translateY(100%)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
-        },
-      },
-    },
-  },
-};
